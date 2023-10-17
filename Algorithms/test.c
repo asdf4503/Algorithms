522266b925bedd0b69e4170599e6e9924dcf79f5
asdf4503 <asdf4503@gwnu.ac.kr>
2023-10-17 19:02:10 +0900

    commit

--- Algorithms/test.c
+++ Algorithms/test.c
@@ -7,12 +7,12 @@
 typedef struct Edge {			//연결리스트 사용을 위한 Edge struct 생성
 	int u; //정점
 	int w; //가중치
-	struct Edge *Link;			//연결리스트 다음 Edge Link (포인터)
+	struct Edge* Link;			//연결리스트 다음 Edge Link (포인터)
 } Edge;
 
 typedef struct Graph {			//Graph struct 생성
 	//int node; //?
-	Edge *head[MAX_SIZE];		//인접리스트
+	Edge* head[MAX_SIZE];		//인접리스트
 } Graph;
 
 typedef struct Heap {			//Heap struct 생성
@@ -20,20 +20,20 @@ typedef struct Heap {			//Heap struct 생성
 	int size;
 } Heap;
 
-void init_graph(Graph *g) {		//그래프 초기화 함수
+void init_graph(Graph* g) {		//그래프 초기화 함수
 	int i = 0;
 	//g->node = 0;
-	for (; i < MAX_SIZE; i++)	
+	for (; i < MAX_SIZE; i++)
 		g->head[i] = NULL;		//MAX_SIZE만큼 그래프 초기화
 }
 
-void init_heap(Heap *heap) {	//힙 초기화 함수
+void init_heap(Heap* heap) {	//힙 초기화 함수
 	heap->size = 0;
 }
 
 
 void addEdge() {
-	Edge* newEdge = (Edge *)malloc(sizeof(Edge)); //Edge에 메모리 동적 할당
+	Edge* newEdge = (Edge*)malloc(sizeof(Edge)); //Edge에 메모리 동적 할당
 }
 
 void delete_min_heap() {
@@ -44,7 +44,7 @@ void decrease_key() {
 
 }
 
-void prim_MST(Graph *g, int u, int w) {
+void prim_MST(Graph* g, int u, int w) {
 
 }
 
