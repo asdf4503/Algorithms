7e77b7bc520d896f7ec1943cc8bcf11e7c183394
asdf4503 <asdf4503@gwnu.ac.kr>
2023-10-18 13:30:20 +0900

    commit

--- Algorithms/test.c
+++ Algorithms/test.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
 #define MAX_SIZE 100
 
@@ -11,7 +10,7 @@ typedef struct Edge {			//연결리스트 사용을 위한 Edge struct 생성
 } Edge;
 
 typedef struct Graph {			//Graph struct 생성
-	//int node; //?
+	int node;
 	Edge* head[MAX_SIZE];		//인접리스트
 } Graph;
 
@@ -22,7 +21,7 @@ typedef struct Heap {			//Heap struct 생성
 
 void init_graph(Graph* g) {		//그래프 초기화 함수
 	int i = 0;
-	//g->node = 0;
+	g->node = 0;
 	for (; i < MAX_SIZE; i++)
 		g->head[i] = NULL;		//MAX_SIZE만큼 그래프 초기화
 }
@@ -32,19 +31,27 @@ void init_heap(Heap* heap) {	//힙 초기화 함수
 }
 
 
-void addEdge() {
-	Edge* newEdge = (Edge*)malloc(sizeof(Edge)); //Edge에 메모리 동적 할당
+void addEdge(Graph *g, int idx, int u, int w) {				//각 정점과 연결된 정점 및 가중치 입력
+	Edge* newEdge = (Edge*)malloc(sizeof(Edge));	//Edge에 메모리 동적 할당
+	newEdge->u = u;
+	newEdge->w = w;
+	newEdge->Link = g->head[idx];
+	g->head[idx] = newEdge;
 }
 
-void delete_min_heap() {
+void insert_min_heap() {	//최소 힙 추가 함수
 
 }
 
-void decrease_key() {
+void delete_min_heap() {	//최소 힙 삭제 함수
 
 }
 
-void prim_MST(Graph* g, int u, int w) {
+void decrease_key() {		//가중치 값 변경 함수
+
+}
+
+void prim_MST(Graph* g, int u, int w) {		//prim algorithm
 
 }
 
@@ -52,5 +59,4 @@ void prim_MST(Graph* g, int u, int w) {
 int main() {
 	Heap heap;
 	init_heap(&heap); //heap 초기화
-	//test
 }
