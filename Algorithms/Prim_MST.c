22d4cce28bc2e34dfe155738e7248cc1215a76a3
asdf4503 <asdf4503@gwnu.ac.kr>
2023-10-30 14:51:28 +0900

    commit

--- Algorithms/Prim_MST.c
+++ Algorithms/Prim_MST.c
@@ -7,29 +7,29 @@
 
 typedef struct Edge {               //연결리스트 사용을 위한 Edge struct 생성
     int u;                          //정점
-    int w;                          
+    int w;                          //가중치
     struct Edge* Link;              //연결리스트 다음 Edge Link (포인터)
 } Edge;
 
 typedef struct Graph {              //Graph struct 생성
-    int node;
+    int node;                       //그래프 정점 개수
     Edge* head[MAX_SIZE];           //인접리스트(adj_list)
 } Graph;
 
 typedef struct HeapNode {           //HeapNode struct 생성
-    int v;                          
-    int d;
+    int v;                          //정점
+    int d;                          //거리
 } HeapNode;
 
 typedef struct Heap {               //Heap struct 생성
     HeapNode data[MAX_SIZE];        //HeapNode의 data 배열 생성
-    int size;
+    int size;                       //Heap의 크기
 } Heap;
 
 void init_graph(Graph* g) {         //그래프 초기화 함수
     int i = 0;
     g->node = 0;
-    for (; i < MAX_SIZE; i++)
+    for ( ; i < MAX_SIZE; i++)
         g->head[i] = NULL;          //MAX_SIZE만큼 그래프 초기화
 }
 
@@ -78,7 +78,7 @@ void decreaseKey(Heap* heap, int v, int d) {                //힙에서 특정 노드의
     for (i = 0; i < heap->size; i++) {
         if (heap->data[i].v == v) {
             heap->data[i].d = d;                            //가중치 값 갱신
-            while (i != 0 && heap->data[i].d < heap->data[(i - 1) / 2].d) { // 가중치 값이 변경된 노드를 올바른 위치로 이동
+            while (i != 0 && heap->data[i].d < heap->data[(i - 1) / 2].d) { //가중치 값이 변경된 노드의 위치 조정
                 swapHeapNode(&heap->data[i], &heap->data[(i - 1) / 2]);
                 i = (i - 1) / 2;
             }
@@ -104,64 +104,63 @@ void addEdge(Graph* g, int idx, int u, int w) {             //각 정점과 연결된 �
     newEdge->Link = g->head[idx];                           //Edge를 인접 리스트 앞에 추가
     g->head[idx] = newEdge;                                 //head 갱신
 
-    printf("%d -> %d   |   가중치 값: %2d |\n", idx, u, w);    // 출력 구문 추가
+    printf("%d -> %d   |   가중치 값: %2d |\n", idx, u, w); // 출력 구문 추가
 }
 
-void prim_MST(Graph* g) {                                   //프림 알고리즘 함수
-    int nearest[MAX_SIZE];
-    int distance[MAX_SIZE];
-    Heap heap;
-    init_heap(&heap);
-    int src = 0;
-    int totalWeight = 0;                                    //prim_MST의 전체 가중치 값을 저장할 변수
+void prim_MST(Graph* g, Heap *heap) {                               //prim 알고리즘을 사용하여 최소 신장 트리를 찾는 함수
+    int nearest[MAX_SIZE];                              //각 정점의 부모 정점을 저장하는 배열
+    int distance[MAX_SIZE];                             //각 정점까지의 최단 거리를 저장하는 배열
+    
+    int src = 0;                                        //시작 정점 번호
+    int totalWeight = 0;                                //최소 신장 트리의 총 가중치 값
 
     int i;
     for (i = 0; i < g->node; i++) {
-        distance[i] = INF;
-        insert_min_heap(&heap, i, INF);                     //모든 노드를 힙에 추가
+        distance[i] = INF;                              //초기 모든 노드의 거리를 무한대로 설정
+        insert_min_heap(heap, i, INF);                 //힙에 모든 정점 추가
     }
 
-    distance[src] = 0;
-    decreaseKey(&heap, src, 0);                             //시작 노드의 가중치 값을 0으로 갱신
+    distance[src] = 0;                                  //시작 노드의 거리를 0으로 설정
+    decreaseKey(heap, src, 0);                         //시작 노드의 거리 갱신
 
     printf("---------prim_MST START!!---------\n\n");
 
-    int count;
-    while (!isEmpty(&heap)) {                               //prim 수행 시작
-        int u = delete_min_heap(&heap).v;
+    int count = 0;                                      //반복 횟수 카운트
+    while (!isEmpty(heap) && count <= g->node - 2) {   //힙이 비어있지 않고, n(정점 수)-2 반복
+        int u = delete_min_heap(heap).v;               //최소값 추출
 
-        Edge* edge = g->head[u];
-        while (edge) {
-            int v = edge->u;
-            if (distance[v] > edge->w) {
-                int oldKey = distance[v]; // 이전 key 값 저장
-                distance[v] = edge->w;
-                decreaseKey(&heap, v, edge->w);
-                nearest[v] = u;
+        Edge* edge = g->head[u];                        //현재 정점의 인접 리스트
+        while (edge) {                                  //인접 리스트 순회
+            int v = edge->u;                            //인접 정점
+            if (distance[v] > edge->w) {                //인접 정점까지의 거리가 현재 가중치보다 클 경우
+                int oldKey = distance[v];               //현재 거리 저장
+                distance[v] = edge->w;                  //거리 갱신
+                decreaseKey(heap, v, edge->w);         //힙에서 정점의 거리 갱신
+                nearest[v] = u;                         //부모 정점 갱신
 
-                // 변경된 key 값과 간선 정보를 함께 출력
-                printf("(%d, %d) - 가중치: %d | ", nearest[v], v, distance[v]);
+                //가중치 값 변경 출력
+                printf("(%d, %d) - 가중치: %d | ", nearest[v], v, distance[v]);            
                 printf("정점 %d의 key값이 %d에서 %d로 변경\n", v, oldKey, distance[v]);
             }
-            edge = edge->Link;
+            edge = edge->Link;                          //다음 인접 정점으로 이동
         }
+        count++;                                        //카운트 증가
     }
 
     printf("\n");
 
-    for (int i = 1; i < g->node; i++) {                     
-        printf("%d -> %d   |   가중치 값 :  %d\n", nearest[i], i, distance[i]);   //prim_MST의 간선과 전체 가중치 출력
-        totalWeight += distance[i];                                             //가중치 값을 totalWeight에 더함
+    //최소 신장 트리의 각 간선과 가중치 출력
+    for (i = 1; i < g->node; i++) {
+        printf("%d -> %d   |   가중치 값 :  %d\n", nearest[i], i, distance[i]);
+        totalWeight += distance[i];                 //총 가중치 값 계산
     }
 
     printf("\n---------prim_MST END!!---------\n");
-    printf("\n전체 가중치 값은 : %d\n", totalWeight);                //전체 가중치 값 출력
+    printf("\n전체 가중치 값은 : %d\n", totalWeight);  
 }
 
-int main() {
-    Heap heap;
-    init_heap(&heap);       //힙 초기화
 
+int main() {
     Graph graph;
     init_graph(&graph);     //그래프 초기화
 
@@ -186,7 +185,10 @@ int main() {
     printf("---------------------------\n");
     printf("\n\n");
 
-    prim_MST(&graph);   //입력 받은 그래프로 prim_MST 실행
+    Heap heap;
+    init_heap(&heap);   //힙 초기화
+
+    prim_MST(&graph, &heap);   //입력 받은 그래프로 prim_MST 실행
 
     return 0;
 }
